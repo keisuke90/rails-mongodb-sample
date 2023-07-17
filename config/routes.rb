5ddbe21c99adb15af246839f186985d1d5eaddf7
KeisukeKuwahara <kuwa.90k@gmail.com>
2023-07-17 23:38:36 +0900

    add views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  get 'home/show'
+
+  root to: "home#index"
+  
+  
+  ## 開発環境用letter_opener
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   resources :comments
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
