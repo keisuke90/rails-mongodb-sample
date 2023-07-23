5028706e3b2ee4147be694226c473c893a6bbeca
KeisukeKuwahara <kuwa.90k@gmail.com>
2023-07-23 16:20:17 +0900

    独自コントローラーを使用するように修正

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,13 @@ Rails.application.routes.draw do
   ## 開発環境用letter_opener
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   
-  devise_for :users
+  devise_for :users, controllers: {
+    confirmations: 'users/confirmations',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    unlocks: 'users/unlocks'
+  }
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
