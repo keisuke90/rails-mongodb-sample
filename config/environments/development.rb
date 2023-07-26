8a7b73770eba8b652fc3f83183776f7cb7d99d06
KeisukeKuwahara <kuwa.90k@gmail.com>
2023-07-27 00:09:55 +0900

    キャッシュストアの設定を修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,19 +19,19 @@ Rails.application.configure do
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-  if Rails.root.join("tmp/caching-dev.txt").exist?
-    config.action_controller.perform_caching = true
-    config.action_controller.enable_fragment_cache_logging = true
+  # if Rails.root.join("tmp/caching-dev.txt").exist?
+  config.action_controller.perform_caching = true
+  config.action_controller.enable_fragment_cache_logging = true
 
-    config.cache_store = :redis_cache_store, { url: 'redis://127.0.0.1:6379/0' }
-    config.public_file_server.headers = {
-      "Cache-Control" => "public, max-age=#{2.days.to_i}"
-    }
-  else
-    config.action_controller.perform_caching = false
+  config.cache_store = :redis_cache_store, { url: 'redis://redis:6379/0' }
+  config.public_file_server.headers = {
+    "Cache-Control" => "public, max-age=#{2.days.to_i}"
+  }
+  # else
+  #   config.action_controller.perform_caching = false
 
-    config.cache_store = :null_store
-  end
+  #   config.cache_store = :null_store
+  # end
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
